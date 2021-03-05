688484cd84c75801e3470e48f2ba9a1d29c909f9
kasiwa-mesi <italynataly@outlook.jp>
2021-03-05 23:22:53 +0900

    creating fetch data from markdown

---
+++ pages/_slug.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="index">
+
+    </div>
+</template>
+
+<script>
+import Article from '~/components/partials/article.vue'
+import fetchData from '~/lib/fetch'
+
+export default {
+    data() {
+        return {
+            article: '',
+        }
+    },
+    components: {
+        Article
+    },
+    async fetch() {
+        console.log(this.$route.params.slug)
+        this.article = await fetchData.getArticle(
+            this.$content,
+            this.params.slug
+        )
+    },
+    head() {
+        return {
+            title: this.article.title,
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: this.article.title,
+                },
+            ],
+        }
+    },
+}
+</script>
