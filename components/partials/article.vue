c7a4f13513755678259de9c7f36d66248b0110f0
kasiwa-mesi <italynataly@outlook.jp>
2021-03-05 16:06:43 +0900

    prepare for fetching data from markdown

---
+++ components/partials/article.vue
@@ -0,0 +1,11 @@
+<template>
+    <article class="article w-auto bg-white my-4 md:mx-4 p-4 rounded-md shadow-md box-border">
+        <nuxt-content :document="data"></nuxt-content>
+    </article>
+</template>
+
+<script>
+export default {
+    props: ['data'],
+}
+</script>
