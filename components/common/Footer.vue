f2d40f71c7c40300f4495212a1401b98dccac3b7
kasiwa-mesi <italynataly@outlook.jp>
2021-03-05 14:33:37 +0900

    fix components for common

---
+++ components/common/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="footer bg-gray-900">
+        <p class="text-center py-16 text-white">&copy; 2021 kasiwa</p>
+    </div>
+</template>
---
+++ components/common/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="header bg-gray-200">
+        <div class="header-title text-center py-8">
+            <h1 class="text-3xl font-bold">
+                <nuxt-link to="/">Kasiwa Blog</nuxt-link>
+            </h1>
+            <p class="text-sm my-4">かしわのブログ</p>
+        </div>
+    </div>
+</template>
